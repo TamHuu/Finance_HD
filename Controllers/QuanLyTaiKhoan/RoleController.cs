cf2962c7ae5d0946e595765d5d84c7528394137a
Huu Tam <Christopherle0902@gmail.com>
2024-10-05 07:30:38 +0700

    up

--- Controllers/QuanLyTaiKhoan/RoleController.cs
+++ Controllers/QuanLyTaiKhoan/RoleController.cs
@@ -1,12 +1,122 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Finance_HD.Helpers;
+using Finance_HD.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Finance_HD.Controllers.QuanLyTaiKhoan
 {
     public class RoleController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public RoleController(ILogger<RoleController> logger, ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var result = _dbContext.SysRole.ToList();
+            return View(result);
         }
+        [HttpGet]
+        public JsonResult getListRole()
+        {
+            var listRole = _dbContext.SysRole
+                             .Where(role => !(role.Deleted ?? false))
+                             .OrderByDescending(role => role.CreatedDate) 
+                             .ToList();
+
+            return Json(new { success = true, Data = listRole });
+        }
+
+        public IActionResult Add()
+        {
+            return View("Form", new SysRole());
+        }
+
+        [HttpPost]
+        public JsonResult Add(SysRole model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Json(new { success = false, message = "Dữ liệu không hợp lệ!" });
+            }
+
+            if (string.IsNullOrWhiteSpace(model.Name))
+            {
+                return Json(new { success = false, message = "Tên role không được để trống!" });
+            }
+
+            var existingRole = _dbContext.SysRole.FirstOrDefault(x => x.Name == model.Name);
+            if (existingRole != null)
+            {
+                return Json(new { success = false, message = "Tên Role đã tồn tại!" });
+            }
+
+            var role = new SysRole
+            {
+                Name = model.Name,
+                Code = model.Code,
+                Description = model.Description,
+                Status = model.Status,
+                CreatedDate = model.CreatedDate ?? DateTime.Now,
+            };
+
+            _dbContext.SysRole.Add(role);
+            _dbContext.SaveChanges();
+
+            return Json(new { success = true, message = "Role đã được thêm thành công!" });
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(string Ma)
+        {
+            var role = _dbContext.SysRole.FirstOrDefault(c => c.Ma == Ma.GetGuid());
+            if (role == null)
+            {
+                return NotFound();
+            }
+            return View("Form", role);
+        }
+        [HttpPost]
+        public JsonResult Edit(SysRole model)
+        {
+            var role = _dbContext.SysRole.FirstOrDefault(x => x.Ma == model.Ma);
+            if (role == null)
+            {
+                return Json(new { success = false, message = "Role này không tồn tại!" });
+            }
+
+
+            role.Name = model.Name;
+            role.Code = model.Code;
+            role.Description = model.Description;
+            role.Status = model.Status;
+            role.UserModified = model.UserModified;
+            role.ModifiedDate = role.ModifiedDate ?? DateTime.Now;
+            _dbContext.SysRole.Update(role);
+            _dbContext.SaveChanges();
+
+            return Json(new { success = true, message = "Cập nhật role thành công!" });
+        }
+        [HttpDelete]
+        public IActionResult Delete(string Id)
+        {
+            var role = _dbContext.SysRole.FirstOrDefault(x => x.Ma == Id.GetGuid());
+            if (role == null)
+            {
+                return Json(new { success = false, message = "Role không tồn tại!" });
+            }
+
+            // Kích hoạt soft delete
+            role.Deleted = true;  // Đánh dấu đã bị xoá
+            role.DeletedDate = DateTime.Now;  // Lưu thời gian xoá
+
+            _dbContext.SysRole.Update(role);  // Cập nhật vào CSDL
+            _dbContext.SaveChanges();
+
+            return Json(new { success = true, message = "Xoá role thành công!" });
+        }
+
     }
 }
