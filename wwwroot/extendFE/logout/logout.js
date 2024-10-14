3e9d079679b7de620be3578081c853f17d886b06
Huu Tam <Christopherle0902@gmail.com>
2024-10-14 08:21:25 +0700

    Update

---
+++ wwwroot/extendFE/logout/logout.js
@@ -0,0 +1,32 @@
+﻿$(document).ready(function () {
+    $('#Logout').on('click', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '/Account/Logout',
+            contentType: "application/json",
+            success: function (response) {
+                let messageTitle = response.success ? 'Thành công!' : 'Thất bại!';
+                let messageIcon = response.success ? 'success' : 'error';
+
+                swal.fire({
+                    title: messageTitle,
+                    text: response.message,
+                    icon: messageIcon
+                }).then(() => {
+                    if (response.success) {
+                        window.location.href = "/";
+                    }
+                });
+            },
+            error: function (xhr, status, error) {
+                swal.fire({
+                    title: 'Đã xảy ra lỗi!',
+                    text: 'Vui lòng thử lại.',
+                    icon: 'error'
+                });
+                console.error('Error: ', error);
+            }
+        });
+    });
+});
